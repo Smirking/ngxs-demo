c02db2bf50cf5968475c8ea25ae578db1ed4fbdb
Ben Atkinson <ben.atkinson@shinesolutions.com>
2019-02-13 14:14:07 +1100

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { DemoState } from './ngxs/demo.state';
+import { DemoStateModel } from './ngxs/demo.model';
+import { IncrementCounter, FetchLego, SetCounter, DecrementCounter } from './ngxs/demo.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ben\'s ngxs-demo thing';
+  constructor(private store: Store) {}
+
+  @Select(DemoState.counter)
+  counter$: Observable<number>;
+
+  @Select((state: DemoStateModel) => state.legoOfTheDay)
+  legoOfTheDay$: Observable<string>;
+
+  increment = () => {
+      this.store.dispatch(new IncrementCounter());
+  }
+  decrement = () => {
+      this.store.dispatch(new DecrementCounter());
+  }
+  reset = () => {
+      this.store.dispatch(new SetCounter(0));
+  }
+  fetchLego = () => {
+      this.store.dispatch(new FetchLego());
+  }
 }
